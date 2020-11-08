7f91b917c0c8efe2ef293f34f64d6fc3bac7d75c
Haato3o <matheus_mrp@hotmail.com>
2020-11-08 00:09:33 -0300

    add show nearby cells

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -7,6 +7,7 @@
 #define FLAG_ICON L'!'
 #define WALL_ICON L'█'
 #define HIDDEN_ICON L'░'
+#define EMPTY_ICON L' '
 
 using namespace std;
 
@@ -19,6 +20,81 @@ struct Slot
 	bool IsBomb;
 	bool IsShown;
 };
+
+void recursive_check_bounds(Slot* map, int posX, int posY, int mWidth, int mHeight)
+{
+
+	if (map[posY * mWidth + posX].IsShown || map[posY * mWidth + posX].Character != EMPTY_ICON)
+	{
+		return;
+	} else
+	{
+		map[posY * mWidth + posX].IsShown = true;
+	}
+
+	// Check top		
+	if (posY > 0)
+	{
+		if (map[(posY - 1) * mWidth + posX].Character == EMPTY_ICON)
+		{
+			recursive_check_bounds(map, posX, posY - 1, mWidth, mHeight);
+		}
+
+		if (posX > 0)
+		{
+			if (map[(posY - 1) * mWidth + (posX - 1)].Character == EMPTY_ICON)
+			{
+				recursive_check_bounds(map, posX - 1, posY - 1, mWidth, mHeight);
+			}
+		}
+		if (posX < mWidth - 1)
+		{
+			if (map[(posY - 1) * mWidth + (posX + 1)].Character == EMPTY_ICON)
+			{
+				recursive_check_bounds(map, posX + 1, posY - 1, mWidth, mHeight);
+			}
+		}
+	}
+
+	// Check bottom
+	if (posY < mHeight - 1)
+	{
+		if (map[(posY + 1) * mWidth + posX].Character == EMPTY_ICON)
+		{
+			recursive_check_bounds(map, posX, posY + 1, mWidth, mHeight);
+		}
+
+		if (posX > 0)
+		{
+			if (map[(posY + 1) * mWidth + (posX - 1)].Character == EMPTY_ICON)
+			{
+				recursive_check_bounds(map, posX - 1, posY + 1, mWidth, mHeight);
+			}
+
+			if (map[(posY + 1) * mWidth + (posX + 1)].Character == EMPTY_ICON)
+			{
+				recursive_check_bounds(map, posX + 1, posY + 1, mWidth, mHeight);
+			}
+		}
+	}
+
+	if (posX > 0)
+	{
+		if (map[posY * mWidth + (posX - 1)].Character == EMPTY_ICON)
+		{
+			recursive_check_bounds(map, posX - 1, posY, mWidth, mHeight);
+		}
+	}
+	
+	if (posX < mWidth - 1)
+	{
+		if (map[posY * mWidth + (posX + 1)].Character == EMPTY_ICON)
+		{
+			recursive_check_bounds(map, posX + 1, posY, mWidth, mHeight);
+		}
+	}
+
+}
 
 Slot* generate_map(int nBombs, int mWidth, int mHeight)
 {
@@ -104,7 +180,7 @@ Slot* generate_map(int nBombs, int mWidth, int mHeight)
 
 			if (bCounter == 0)
 			{
-				map[pos].Character = L' ';
+				map[pos].Character = EMPTY_ICON;
 			} else
 			{
 				map[pos].Character = (wchar_t)(0x30 + bCounter);
@@ -142,7 +218,7 @@ int main()
 
 	int mMapWidth = 46;
 	int mMapHeight = 16;
-	int mNumberBombs = 10;
+	int mNumberBombs = 40;
 
 	Slot* mineMap = generate_map(mNumberBombs, mMapWidth, mMapHeight);
 
@@ -151,7 +227,9 @@ int main()
 	bool IsLeftButtonPressed;
 	bool IsRightButtonPressed;
 
-	while (true)
+	bool bGameOver = false;
+
+	while (!bGameOver)
 	{
 		IsLeftButtonPressed = false;
 		IsRightButtonPressed = false;
@@ -212,7 +290,16 @@ int main()
 
 		if (IsLeftButtonPressed && screen[mPosition.Y * ScreenWidth + mPosition.X] != WALL_ICON)
 		{
-			mineMap[(mPosition.Y - 2) * mMapWidth + (mPosition.X - 2)].IsShown = true;
+			int position = (mPosition.Y - 2) * mMapWidth + (mPosition.X - 2);
+
+			if (!mineMap[position].IsShown && mineMap[position].Character == EMPTY_ICON)
+			{
+				recursive_check_bounds(mineMap, mPosition.X, mPosition.Y, mMapWidth, mMapHeight);
+			} else if (!mineMap[position].IsBomb)
+			{
+				mineMap[position].IsShown = true;
+			}
+			
 		}
 
 		WriteConsoleOutputCharacter(hConsole, screen, ScreenWidth * ScreenHeight, { 0, 0 }, &dwBytesWritten);
